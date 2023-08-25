4beb50147be64178bbef87633efd30045734693c
wygrajr <wycliffe.graftonjr@gmail.com>
2023-08-25 15:43:40 -0400

    updated paths adter sign in/sign up. Created and connected Post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Project = require('./Project');
+const Post = require('./Post');
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
@@ -10,4 +11,13 @@ Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project, Post };
