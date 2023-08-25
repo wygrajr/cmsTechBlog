5a9c7811a816ba0943cc00e3588456aec2f106eb
wygrajr <wycliffe.graftonjr@gmail.com>
2023-08-25 19:00:51 -0400

    edit post handlebar created and connected with dashboard and homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const { Project, User, Post } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
@@ -14,10 +13,8 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
     res.render('homepage', { 
       posts, 
       logged_in: req.session.logged_in 
@@ -27,9 +24,9 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get('/edit/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
@@ -38,10 +35,11 @@ router.get('/project/:id', async (req, res) => {
       ],
     });
 
-    const project = projectData.get({ plain: true });
+    const post = postData.get({ plain: true });
+    console.log(post)
 
-    res.render('project', {
-      ...project,
+    res.render('editpost', {
+      post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -49,23 +47,6 @@ router.get('/project/:id', async (req, res) => {
   }
 });
 
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 router.get('/dashboard', withAuth, async (req, res) => {
   try {
